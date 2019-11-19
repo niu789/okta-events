40744acbe1f61b064624b32d6bc580c740215fe3
Nicolas Belanger <niu789@gmail.com>
2019-11-19 10:06:09 -0500

    Changed the script to use the new API and to return from a certain time

--- oktaEvents.py
+++ oktaEvents.py
@@ -6,20 +6,28 @@ import sys
 
 org = sys.argv[1]
 token = sys.argv[2]
-relative_cutoff_secs = sys.argv[3]
+startTime = sys.argv[3]
 
-url = 'https://' + org + '.okta.com/api/v1/events'
+#url = 'https://' + org + '.okta.com/api/v1/events'
+url = 'https://' + org + '.okta.com/api/v1/logs'
 headers = { 'Authorization': 'SSWS ' + token }
-now = datetime.now()
-startTime = now - timedelta(0,int(relative_cutoff_secs))
+#now = datetime.now()
+
 # Okta API breaks with microseconds
-params = { 'startDate': startTime.isoformat()[:-3] + 'Z' }
+#params = { 'startDate': startTime.isoformat()[:-3] + 'Z' }
+#params = { 'startDate': startTime[:-3] + 'Z' }
+params = { 'since': startTime[:-3] + 'Z' }
 
 events = requests.get(url, headers=headers, params=params)
+#events.text
+i = 0
 for e in events.json():
-    print (e)
+    if i == 0:
+        i += 1
+        continue
+    print (json.dumps(e))
 
-while 'next' in events.links:
-    events = requests.get(events.links['next']['url'], headers=headers)
-    for e in events.json():
-        print e
+#while 'next' in events.links:
+#    events = requests.get(events.links['next']['url'], headers=headers)
+#    for e in events.json():
+#        print (e)
